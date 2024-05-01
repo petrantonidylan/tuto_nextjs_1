c719670ae95feb6d7cfd9c8219de3498b623fbe3
Dylan Petrantoni <dylan.petrantoni@cci.com>
2024-05-01 14:38:36 +0200

    commit_3

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 
 export const metadata = {
     title: "Promptopia",
@@ -9,13 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
-            <div className="main">
-                <div className="gradient"/>
-            </div>
+            <Provider>
+                <div className="main">
+                    <div className="gradient"/>
+                </div>
 
-            <main className="app">
-                {children}
-            </main>
+                <main className="app">
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
