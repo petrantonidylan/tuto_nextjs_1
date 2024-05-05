04d438cd339ddbc1a91a352b8420977a2f840a65
Dylan Petrantoni <dylan.petrantoni@cci.com>
2024-05-05 14:55:26 +0200

    commit_4

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -28,7 +28,7 @@ const Nav = () => {
       </Link>
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -40,7 +40,7 @@ const Nav = () => {
 
             <Link href="/profile" >
               <Image 
-                src="/assets/images/logo.svg" 
+                src={session?.user.image}
                 width={37} 
                 height={37} 
                 className="rounded-full" 
@@ -62,10 +62,10 @@ const Nav = () => {
 
       <div className="sm:hidden flex relative">
         {
-          isUserLoggedIn ? (
+          session?.user ? (
             <div className="flex">
               <Image 
-                src="/assets/images/logo.svg" 
+                src={session?.user.image}
                 width={37} height={37} 
                 className="rounded-full" 
                 alt="profile" 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    } 
+};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//       serverComponentsExternalPackages: ["mongoose"],
+//     },
+//     images: {
+//       domains: ['lh3.googleusercontent.com'],
+//     },
+//     webpack(config) {
+//       config.experiments = {
+//         ...config.experiments,
+//         topLevelAwait: true,
+//       }
+//       return config
+//     }
+//   }
+  
+//   module.exports = nextConfig
